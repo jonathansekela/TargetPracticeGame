c3eedb60b0a97d504be48ca0f71ab0c7090e2276
Jonathan Sekela <jonathan.sekela@gmail.com>
2024-01-19 15:44:13 -0500

    Add bones of main

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import pygame as pg
+from pygame import mixer
+import random
+
+pg.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+pg.init()
+
+# region general setup
+
+SCREEN_WIDTH = 928
+SCREEN_HEIGHT = 793
+BACKGROUND = (202, 228, 241)  # @todo: turn this into something worthwhile
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption('Target Practice')
+random.seed()
+
+game_running = True
+menu_running = True
+# endregion
+
+# region game loop
+while game_running:
+	# update background
+	screen.blit(BACKGROUND, (0, 0))
+	
+	# event handler
+	for event in pg.event.get():
+		if event.type == pg.QUIT:
+			game_running = False
+	
+	pg.display.update()
+# endregion
+
+pg.quit()
